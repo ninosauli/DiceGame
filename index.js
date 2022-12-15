9164914aa7c542c2ea1c571d6dda24127c329615
nino <118080061+ninosauli@users.noreply.github.com>
2022-12-16 00:02:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 function getRandom() {
   // Generate Random number
-  var randomNumber1 = Math.random();
-  randomNumber1 = Math.floor((randomNumber1 * 6) + 1);
+  var randomNumber1 = Math.floor( Math.random() * 6) + 1;
 
-  var randomNumber2 = Math.random();
-  randomNumber2 = Math.floor(randomNumber2 * 6 + 1);
 
 // Select the images based on the random number
   document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+  var randomNumber2 = Math.floor( Math.random() * 6) + 1;
+
   document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 
 
 
-    if (randomNumber1 > randomNumber2) {
-      document.querySelector("h1").textContent = "🎲 Player 1 Wins";
-    } else if (randomNumber1 < randomNumber2) {
-      document.querySelector("h1").textContent = "Player 2 Wins 🎲" ;
-    } else {
-      document.querySelector("h1").textContent = " 🎲 Draw 🎲";
-    }
- 
-    
-}
-getRandom()
 
+      if (randomNumber1 > randomNumber2) {
+      document.querySelector("h1").textContent = "🎲 Player 1 Wins";
+    } else if (randomNumber1 === randomNumber2) {
+      document.querySelector("h1").textContent = "🎲 Draw 🎲" ;
+    } else {
+      document.querySelector("h1").textContent = " Player 2 Wins 🎲";
+    }
+
+
+
+}
+getRandom();
 
 // Change H1 ONLY after Refresh is clicked
-  document.getElementById("refresh-button").onclick = getRandom() 
-
-
+document.getElementById("refresh-button").onclick = getRandom();
 
 
 // Button refreshing the funciton getRandom()
