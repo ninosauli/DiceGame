f9eb3155cc9b33a69a73bc4d01913c096f2997b6
nino <118080061+ninosauli@users.noreply.github.com>
2022-12-16 00:16:03 +0100

    last

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ function getRandom() {
   // Generate Random number
   var randomNumber1 = Math.floor( Math.random() * 6) + 1;
 
+  var randomDiceImage = "dice" + randomNumber1 + ".png;
+  var randomIamgeSource = "images/" + randomDiceImage;
 
 // Select the images based on the random number
-  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document.querySelector(".img1").setAttribute("src", randomIamgeSource);
 
   var randomNumber2 = Math.floor( Math.random() * 6) + 1;
+    var randomIamgeSource2 = "images/dice" + randomNumber2 + ".png";
 
-  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+  document.querySelector(".img2").setAttribute("src", randomIamgeSource2);
 
 
 
